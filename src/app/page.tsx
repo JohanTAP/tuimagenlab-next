accfb71c7acc0b8fecc02ff40c0af35328d7f1ec
Johan Gutierrez <johgutierrez22@cftsa.cl>
2025-02-07 17:58:16 -0300

    Agregar componente Footer a la página de inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { Footer } from "@/components/footer";
 import Hero from "@/components/hero";
 
 export default function Inicio ()
 {
   return (
-    <Hero />
+    <>
+      <Hero />
+      <Footer />
+    </>
   );
 }
